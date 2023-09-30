ca369b6bec45db3f9a61b1a4db2b9099f9473c64
Vinayak <m.vinayak2005@gmail.com>
2023-09-30 22:57:40 +0530

    Booking ticket according age

---
+++ 04_ticketRateApp.js
@@ -0,0 +1,9 @@
+let numberOfChilds = 2;
+let numberOfAdults = 1;
+let numberOfSeniours = 1;
+
+numberOfChilds = numberOfChilds * 100;
+numberOfAdults = numberOfAdults * 150;
+numberOfSeniours = numberOfSeniours * 120;
+
+console.log("The total ticket price : ",numberOfChilds+numberOfAdults+numberOfSeniours);
