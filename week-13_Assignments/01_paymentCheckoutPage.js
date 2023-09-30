b777b5f6d5537a286da9ccf47886266415cb3e4f
Vinayak <m.vinayak2005@gmail.com>
2023-09-30 23:06:35 +0530

    Discount Calculator

--- week-13_Assignments/01_paymentCheckoutPage.js
+++ week-13_Assignments/01_paymentCheckoutPage.js
@@ -3,9 +3,9 @@ let discountPercentage = 20;
 
 if(discountPercentage != 0){
     let mrp = (totalValue * discountPercentage) / 100;
-    console.log("The final price after discount : ",totalValue-mrp);
+    console.log("The final price after discount : "+totalValue-mrp);
 }
 else{
     console.log("Sorry you have no discount");
-    console.log("The final price : ",totalValue);
+    console.log("The final price : "+totalValue);
 }
